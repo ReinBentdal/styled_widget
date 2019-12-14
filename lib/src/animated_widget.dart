b4ebbe38990f02c654335f62e048b8ab4d9c0076
Rein Gundersen Bentdal <rein.bent@gmail.com>
2019-12-14 14:29:48 +0100

    renamed widget animation

--- lib/src/animated_widget.dart
+++ lib/src/animated_widget.dart
@@ -1,10 +1,9 @@
-import 'package:flutter/foundation.dart';
-import 'package:flutter/rendering.dart';
-import 'package:flutter/widgets.dart';
+part of 'widget.dart';
 
-class AnimatedDecorationBox extends ImplicitlyAnimatedWidget {
+
+class _AnimatedDecorationBox extends ImplicitlyAnimatedWidget {
   /// The [curve] and [duration] arguments must not be null.
-  AnimatedDecorationBox({
+  _AnimatedDecorationBox({
     Key key,
     this.decoration,
     this.position,
@@ -13,7 +12,12 @@ class AnimatedDecorationBox extends ImplicitlyAnimatedWidget {
     @required Duration duration,
     VoidCallback onEnd,
   })  : assert(decoration == null || decoration.debugAssertIsValid()),
-        super(key: key, curve: curve, duration: duration, onEnd: onEnd);
+        super(
+          key: key,
+          curve: curve,
+          duration: duration,
+          onEnd: onEnd,
+        );
 
   /// The [child] contained by the container.
   ///
@@ -42,11 +46,12 @@ class AnimatedDecorationBox extends ImplicitlyAnimatedWidget {
     super.debugFillProperties(properties);
     properties.add(
         DiagnosticsProperty<Decoration>('bg', decoration, defaultValue: null));
+    // TODO: debug [position]?
   }
 }
 
 class _AnimatedDecorationBoxState
-    extends AnimatedWidgetBaseState<AnimatedDecorationBox> {
+    extends AnimatedWidgetBaseState<_AnimatedDecorationBox> {
   DecorationTween _decoration;
 
   @override
@@ -72,9 +77,9 @@ class _AnimatedDecorationBoxState
   }
 }
 
-class AnimatedConstrainedBox extends ImplicitlyAnimatedWidget {
+class _AnimatedConstrainedBox extends ImplicitlyAnimatedWidget {
   /// The [curve] and [duration] arguments must not be null.
-  AnimatedConstrainedBox({
+  _AnimatedConstrainedBox({
     Key key,
     this.constraints,
     this.child,
@@ -82,7 +87,12 @@ class AnimatedConstrainedBox extends ImplicitlyAnimatedWidget {
     @required Duration duration,
     VoidCallback onEnd,
   })  : assert(constraints == null || constraints.debugAssertIsValid()),
-        super(key: key, curve: curve, duration: duration, onEnd: onEnd);
+        super(
+          key: key,
+          curve: curve,
+          duration: duration,
+          onEnd: onEnd,
+        );
 
   /// The [child] contained by the container.
   ///
@@ -115,7 +125,7 @@ class AnimatedConstrainedBox extends ImplicitlyAnimatedWidget {
 }
 
 class _AnimatedConstrainedBoxState
-    extends AnimatedWidgetBaseState<AnimatedConstrainedBox> {
+    extends AnimatedWidgetBaseState<_AnimatedConstrainedBox> {
   BoxConstraintsTween _constraints;
 
   @override
@@ -141,11 +151,11 @@ class _AnimatedConstrainedBoxState
   }
 }
 
-class AnimatedTransform extends ImplicitlyAnimatedWidget {
+class _AnimatedTransform extends ImplicitlyAnimatedWidget {
   /// Creates a container that animates its parameters implicitly.
   ///
   /// The [curve] and [duration] arguments must not be null.
-  AnimatedTransform({
+  _AnimatedTransform({
     this.transform,
     this.origin,
     this.alignment,
@@ -153,7 +163,9 @@ class AnimatedTransform extends ImplicitlyAnimatedWidget {
     this.child,
     Curve curve = Curves.linear,
     @required Duration duration,
-  }) : super(curve: curve, duration: duration);
+  }) : super(
+            curve: curve,
+            duration: duration,);
 
   /// The [child] contained by the container.
   ///
@@ -184,11 +196,12 @@ class AnimatedTransform extends ImplicitlyAnimatedWidget {
         'alignment', alignment,
         showName: false, defaultValue: null));
     properties.add(ObjectFlagProperty<Matrix4>.has('transform', transform));
+    // TODO: debug [origin], [transformHitTest]?
   }
 }
 
 class _AnimatedTransformState
-    extends AnimatedWidgetBaseState<AnimatedTransform> {
+    extends AnimatedWidgetBaseState<_AnimatedTransform> {
   AlignmentGeometryTween _alignment;
   Matrix4Tween _transform;
 
@@ -219,5 +232,155 @@ class _AnimatedTransformState
         showName: false, defaultValue: null));
     description
         .add(ObjectFlagProperty<Matrix4Tween>.has('transform', _transform));
+  }
+}
+
+class _AnimatedBorderRadius extends ImplicitlyAnimatedWidget {
+  /// The [curve] and [duration] arguments must not be null.
+  _AnimatedBorderRadius({
+    Key key,
+    this.topLeft,
+    this.topRight,
+    this.bottomLeft,
+    this.bottomRight,
+    this.child,
+    Curve curve = Curves.linear,
+    @required Duration duration,
+    VoidCallback onEnd,
+  }) : super(
+          key: key,
+          curve: curve,
+          duration: duration,
+          onEnd: onEnd,
+        );
+
+  /// The [child] contained by the container.
+  ///
+  /// If null, and if the [constraints] are unbounded or also null, the
+  /// container will expand to fill all available space in its parent, unless
+  /// the parent provides unbounded constraints, in which case the container
+  /// will attempt to be as small as possible.
+  ///
+  /// {@macro flutter.widgets.child}
+  final Widget child;
+
+  final double topLeft;
+  final double topRight;
+  final double bottomLeft;
+  final double bottomRight;
+
+  @override
+  _AnimatedBorderRadiusState createState() => _AnimatedBorderRadiusState();
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    //TODO: debug [topLeft], [topRight], [bottomLeft], [bottomRight]
+  }
+}
+
+class _AnimatedBorderRadiusState
+    extends AnimatedWidgetBaseState<_AnimatedBorderRadius> {
+  Tween<double> _topLeft;
+  Tween<double> _topRight;
+  Tween<double> _bottomLeft;
+  Tween<double> _bottomRight;
+
+  @override
+  void forEachTween(TweenVisitor<dynamic> visitor) {
+    _topLeft = visitor(_topLeft, widget.topLeft,
+        (dynamic value) => Tween<double>(begin: value));
+    _topRight = visitor(_topRight, widget.topRight,
+        (dynamic value) => Tween<double>(begin: value));
+    _bottomLeft = visitor(_bottomLeft, widget.bottomLeft,
+        (dynamic value) => Tween<double>(begin: value));
+    _bottomRight = visitor(_bottomRight, widget.bottomRight,
+        (dynamic value) => Tween<double>(begin: value));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      child: widget.child,
+      borderRadius: BorderRadius.only(
+        topLeft: Radius.circular(_topLeft.evaluate(animation)),
+        topRight: Radius.circular(_topRight.evaluate(animation)),
+        bottomLeft: Radius.circular(_bottomLeft.evaluate(animation)),
+        bottomRight: Radius.circular(_bottomRight.evaluate(animation)),
+      ),
+    );
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder description) {
+    super.debugFillProperties(description);
+    //TODO: debug [_topLeft], [_topRight], [_bottomLeft], [_bottomRight]
+  }
+}
+
+class _AnimatedBackgroundBlur extends ImplicitlyAnimatedWidget {
+  /// Creates a widget that animates its opacity implicitly.
+  ///
+  /// The [opacity] argument must not be null and must be between 0.0 and 1.0,
+  /// inclusive. The [curve] and [duration] arguments must not be null.
+  const _AnimatedBackgroundBlur({
+    Key key,
+    this.child,
+    @required this.sigma,
+    Curve curve = Curves.linear,
+    @required Duration duration,
+    VoidCallback onEnd,
+    this.alwaysIncludeSemantics = false,
+  }) : assert(sigma != null && sigma >= 0.0),
+       super(key: key, curve: curve, duration: duration, onEnd: onEnd,);
+
+  /// The widget below this widget in the tree.
+  ///
+  /// {@macro flutter.widgets.child}
+  final Widget child;
+
+  final double sigma;
+
+  /// Whether the semantic information of the children is always included.
+  ///
+  /// Defaults to false.
+  ///
+  /// When true, regardless of the opacity settings the child semantic
+  /// information is exposed as if the widget were fully visible. This is
+  /// useful in cases where labels may be hidden during animations that
+  /// would otherwise contribute relevant semantics.
+  final bool alwaysIncludeSemantics;
+
+  @override
+  _AnimatedBackgroundBlurState createState() => _AnimatedBackgroundBlurState();
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties.add(DoubleProperty('background blur', sigma));
+  }
+}
+
+class _AnimatedBackgroundBlurState extends ImplicitlyAnimatedWidgetState<_AnimatedBackgroundBlur> {
+  Tween<double> _sigma;
+  Animation<double> _sigmaAnimation;
+
+  @override
+  void forEachTween(TweenVisitor<dynamic> visitor) {
+    _sigma = visitor(_sigma, widget.opacity, (dynamic value) => Tween<double>(begin: value));
+  }
+
+  @override
+  void didUpdateTweens() {
+    _sigmaAnimation = animation.drive(_sigma);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FadeTransition(
+      opacity: _sigmaAnimation,
+      child: widget.child,
+      alwaysIncludeSemantics: widget.alwaysIncludeSemantics,
+    );
   }
 }
