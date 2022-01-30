b467bb6335f3921862da043de34d0ca96db8382a
chomosuke <a13323600@gmail.com>
2022-01-30 17:17:37 +1100

    add material

--- lib/src/extensions/widget_extension.dart
+++ lib/src/extensions/widget_extension.dart
@@ -1384,4 +1384,32 @@ extension StyledWidget on Widget {
         maxHeight: maxHeight,
         child: this,
       );
+
+  Widget material({
+    Key? key,
+    MaterialType type = MaterialType.canvas,
+    double elevation = 0.0,
+    Color? color,
+    Color? shadowColor,
+    TextStyle? textStyle,
+    BorderRadiusGeometry? borderRadius,
+    ShapeBorder? shape,
+    bool borderOnForeground = true,
+    Clip clipBehavior = Clip.none,
+    Duration animationDuration = kThemeChangeDuration,
+  }) =>
+      Material(
+        key: key,
+        type: type,
+        elevation: elevation,
+        color: color,
+        shadowColor: shadowColor,
+        textStyle: textStyle,
+        borderRadius: borderRadius,
+        shape: shape,
+        borderOnForeground: borderOnForeground,
+        clipBehavior: clipBehavior,
+        animationDuration: animationDuration,
+        child: this,
+      );
 }
